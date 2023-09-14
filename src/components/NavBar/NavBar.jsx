10e46e970cb1f185499a967f7b0fe4f1f5b275f9
Sebastian Gonzalez <sg3072042@gmail.com>
2023-09-14 19:53:00 -0300

    corrigiendo problema de sitaxis

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,63 +5,63 @@ import "./style.css";
 export default function NavBar() {
   return (
     <div>
-      {/* menu de navegacion responsive  */}
-      <nav class="navbar navbar-expand-md">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="index.html">
+      {/* menú de navegación responsive */}
+      <nav className="navbar navbar-expand-md">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="index.html">
             WorldShoes
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="offcanvas"
             data-bs-target="#offcanvasDarkNavbar"
             aria-controls="offcanvasDarkNavbar"
             aria-label="Toggle navigation"
           >
-            <i class="bi bi-list"></i>
+            <i className="bi bi-list"></i>
           </button>
           <div
-            class="offcanvas offcanvas-end"
-            tabindex="-1"
+            className="offcanvas offcanvas-end"
+            tabIndex="-1"
             id="offcanvasDarkNavbar"
             aria-labelledby="offcanvasDarkNavbarLabel"
           >
-            <div class="offcanvas-header">
-              <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">
                 Menu
               </h5>
               <button
                 type="button"
-                class="btn-close"
+                className="btn-close"
                 data-bs-dismiss="offcanvas"
                 aria-label="Close"
               ></button>
             </div>
-            <div class="offcanvas-body">
-              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li class="nav-item">
-                  <a class="nav-link" href="index.html">
+            <div className="offcanvas-body">
+              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li className="nav-item">
+                  <a className="nav-link" href="index.html">
                     Inicio
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="./">
+                <li className="nav-item">
+                  <a className="nav-link" href="./">
                     Nike
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="./">
+                <li className="nav-item">
+                  <a className="nav-link" href="./">
                     Adidas
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="./">
+                <li className="nav-item">
+                  <a className="nav-link" href="./">
                     Puma
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="./">
+                <li className="nav-item">
+                  <a className="nav-link" href="./">
                     Nosotros
                   </a>
                 </li>
